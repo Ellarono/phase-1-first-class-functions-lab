56f490426f56a8990dbcefedba883b6bae13425e
Daniella Rono <daniellarono@gmail.com>
2024-07-15 01:15:52 +0300

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2)
+  }
+  const selectingDrivers =[
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+  ]
+  function createFareMultiplier(fareMultiplier){
+
+    return function(fare){return fare * fareMultiplier
+    
+    }
+  }
+  function fareDoubler(fare){
+    return fare*2
+  }
+  function  fareTripler(fare){
+    return fare * 3
+  }
+  const selectDifferentDrivers = function(drivers, selectionFunction) {
+    return selectionFunction(drivers);
+  };
